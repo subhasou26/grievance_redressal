3893f4999bebbe3b9d142c2072b7401b0fd931ca
subha <subhadipp26@gmail.com>
2024-10-17 09:00:55 +0530

    email_functio update

--- utils/mail.js
+++ utils/mail.js
@@ -1,33 +1,19 @@
-const jwt = require("jsonwebtoken");
+
 const nodemailer = require("nodemailer");
-const { google } = require("googleapis");
+
 require("dotenv").config();
 
 
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const REFRESE_TOKEN = process.env.REFRESE_TOKEN;
-const ACCES_TOKEN=process.env.ACCES_TOKEN;
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
-);
-oAuth2Client.setCredentials({ access_token:ACCES_TOKEN });
+
 async function sendMail(mailOptions) {
   try {
-    const accessToken = await oAuth2Client.getAccessToken();
+   
     const transport = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        type: "OAuth2",
-        user: mailOptions.from,
-        clientId: CLIENT_ID,
-        clientSecret: CLIENT_SECRET,
-        refreshToken: REFRESE_TOKEN,
-        accessToken: accessToken,
+        user:process.env.EMAIL,
+        pass:process.env.PASS
       },
     });
 
