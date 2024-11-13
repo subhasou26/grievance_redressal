c95fe31dcb71001bf38288141ddb0a0087d20435
Subhadip Paul <83156210+subhasou26@users.noreply.github.com>
2024-11-13 15:59:53 +0530

    Ec2

--- index.js
+++ index.js
@@ -57,4 +57,4 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { statusCode, message });
 });
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
